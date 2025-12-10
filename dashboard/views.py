c28c5b3ef348fb7a45359164101a7ffa4e17a7ff
oAmorim2k5 <victorhuski@gmail.com>
2025-12-10 17:39:01 -0300

    Creat dashboard app, frontend home page with sidebar

---
+++ dashboard/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def admin_home(request):
+    return render(request, 'admin_home.html')
+
+def client_home(request):
+    return render(request, 'client_home.html')
